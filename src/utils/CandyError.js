4b1f0ae6ed86c77e1725a4185cbdc8cd0f13ce28
Rose <84075133+RoseAmelia@users.noreply.github.com>
2021-05-21 00:03:18 +0600

    Errors

---
+++ src/utils/CandyError.js
@@ -0,0 +1,7 @@
+const Errors = {
+    TOKEN_MISSING: "Token is missing",
+    TOKEN_INVALID: "An invalid token was provided",
+    FAILED_LOG: "Couldn't able to send log"
+};
+
+module.exports = Errors;
