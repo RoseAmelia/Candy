52e53766a15f3282ee43d316233c826b7cedc542
Rose <84075133+RoseAmelia@users.noreply.github.com>
2021-05-21 17:02:38 +0600

    Update CandyError.js

--- src/utils/CandyError.js
+++ src/utils/CandyError.js
@@ -1,6 +1,8 @@
 const Errors = {
     TOKEN_MISSING: "Token is missing",
     TOKEN_INVALID: "An invalid token was provided",
+    MONGO_URL_MISSING: "Mongo Url is missing",
+    MONGO_URL_INVALID: "An invalid url was provided",
     FAILED_LOG: "Couldn't able to send log"
 };
 
